c202f97257fce69bee4b7e762a2a8ad6a995cd5a
Kasparas Anusauskas <kasparas.anusauskas@gmail.com>
2021-11-09 22:42:14 +0200

    Add test images and rarity level generation

--- test1.js
+++ test1.js
@@ -1,27 +1,68 @@
-const puppeteer = require('puppeteer');
-let iterations = 0;
+const puppeteer = require("puppeteer");
+const fs = require("fs");
 
-(async () => {
-    const browser = await puppeteer.launch({ headless: false });
-    const page = await browser.newPage();
-    
-    async function submitFormOnce() {
-        await page.goto('http://localhost:3000');
+const imageData = [];
 
-        await page.type('#name', 'Pavadinimas 1');
-        await page.type('#description', 'Aprasymas 1');
-        await page.click('#upload');
+function getRarityData(attributes) {
+  console.log("attributes", attributes);
+  // attribute shape is
+  // {
+  //     "trait_type": "background",
+  //     "value": "background1"
+  //   },
+  let totalRarityLvl = 0;
+  const rarityData = attributes.map((attr) => {
+    const rarityLvl = parseInt(attr.value.replace(attr.trait_type, "") || '0');
+    totalRarityLvl += rarityLvl;
+    return { name: attr.trait_type, rarityLvl };
+  });
+  return {
+    lvl: totalRarityLvl,
+    data: rarityData,
+  };
+}
 
-        page
-            .waitForSelector('#success')
-            .then(async () => {
-                await page.goto('http://localhost:3000/upload');
-                iterations++;
-                if (iterations > 5) return;
-                submitFormOnce();
-            })
-    }
+async function loadImages(path) {
+  const imageDir = `${path}/images`;
+  const jsonDir = `${path}/json`;
+  const files = await fs.promises.opendir(`${path}/images`);
+  for await (const file of files) {
+    let jsonRawData = fs.readFileSync(
+      `${jsonDir}/${file.name.split(".")[0]}.json`
+    );
+    let jsonData = JSON.parse(jsonRawData);
+    imageData.push({
+      file: `${imageDir}/${file.name}`,
+      jsonData,
+      rarityData: getRarityData(jsonData.attributes),
+    });
+  }
+  console.log(imageData.map(d => d.rarityData));
+}
 
-    submitFormOnce();
-    //   await browser.close();
-})();
+loadImages("./NFAs").catch(console.error);
+
+// (async () => {
+//     const browser = await puppeteer.launch({ headless: false });
+//     const page = await browser.newPage();
+
+//     async function submitFormOnce() {
+//         await page.goto('http://localhost:3000');
+
+//         await page.type('#name', 'Pavadinimas 1');
+//         await page.type('#description', 'Aprasymas 1');
+//         await page.click('#upload');
+
+//         page
+//             .waitForSelector('#success')
+//             .then(async () => {
+//                 await page.goto('http://localhost:3000/upload');
+//                 iterations++;
+//                 if (iterations > 5) return;
+//                 submitFormOnce();
+//             })
+//     }
+
+//     submitFormOnce();
+//     //   await browser.close();
+// })();
